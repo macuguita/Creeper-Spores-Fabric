1c2ffae04bc09be45b8b427f6f361507253e57ed
macuguita <raulpripri@gmail.com>
2025-03-23 01:14:46 +0100

    fixed on syntra connector

--- src/main/java/org/ladysnake/creeperspores/mixin/WorldMixin.java
+++ src/main/java/org/ladysnake/creeperspores/mixin/WorldMixin.java
@@ -18,30 +18,67 @@
 package org.ladysnake.creeperspores.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.explosion.ExplosionBehavior;
 import org.ladysnake.creeperspores.CreeperGrief;
 import org.ladysnake.creeperspores.CreeperSpores;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nullable;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
     @Shadow public abstract GameRules getGameRules();
 
-    @ModifyVariable(method = "createExplosion(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lnet/minecraft/world/explosion/ExplosionBehavior;DDDFZLnet/minecraft/world/World$ExplosionSourceType;Z)Lnet/minecraft/world/explosion/Explosion;", ordinal = 0, at = @At(value = "STORE", ordinal = 0))
-    private Explosion.DestructionType griefLessExplosion(Explosion.DestructionType explosionType, @Nullable Entity entity) {
+    @Inject(
+            method = "createExplosion(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lnet/minecraft/world/explosion/ExplosionBehavior;DDDFZLnet/minecraft/world/World$ExplosionSourceType;Z)Lnet/minecraft/world/explosion/Explosion;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/World;getDestructionType(Lnet/minecraft/world/GameRules$Key;)Lnet/minecraft/world/explosion/Explosion$DestructionType;"
+            ),
+            cancellable = true
+    )
+    private void griefLessExplosion(
+            @Nullable Entity entity,
+            @Nullable DamageSource damageSource,
+            @Nullable ExplosionBehavior behavior,
+            double x,
+            double y,
+            double z,
+            float power,
+            boolean createFire,
+            World.ExplosionSourceType explosionSourceType,
+            boolean particles,
+            CallbackInfoReturnable<Explosion> cir
+    ) {
         if (entity instanceof CreeperEntity creeper) {
             CreeperGrief grief = this.getGameRules().get(CreeperSpores.CREEPER_GRIEF).get();
             if (!grief.shouldGrief(creeper.shouldRenderOverlay())) {
-                return Explosion.DestructionType.KEEP;
+                // Override the destruction type to KEEP if creeper grief is disabled
+                Explosion explosion = new Explosion(
+                        (World) (Object) this,
+                        entity,
+                        damageSource,
+                        behavior,
+                        x,
+                        y,
+                        z,
+                        power,
+                        createFire,
+                        Explosion.DestructionType.KEEP
+                );
+                explosion.collectBlocksAndDamageEntities();
+                explosion.affectWorld(particles);
+                cir.setReturnValue(explosion); // Return the modified explosion
             }
         }
-        return explosionType;
     }
 }
